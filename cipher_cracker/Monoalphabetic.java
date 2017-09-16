6de38ee13d30554a2791b194bbe6e3994310d2c8
prijatelj <theomnipotentwoof@gmail.com>
2017-09-15 23:12:03 -0400

    initial commit, added cipher project beginnings

---
+++ cipher_cracker/Monoalphabetic.java
@@ -0,0 +1,15 @@
+/**
+ * Java program to crack monoalphabetic ciphers.
+ *
+ * @author Derek S. Prijatelj
+ */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Monoalphabetic{
+
+    public static void main(String[] args){
+
+    }
+}
