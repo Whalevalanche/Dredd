139396506b58e42eaf8e74c466641be62050bebf
prijatelj <theomnipotentwoof@gmail.com>
2017-09-16 13:16:04 -0400

    added beginnings of brute force decypher

--- cipher_cracker/Monoalphabetic.java
+++ cipher_cracker/Monoalphabetic.java
@@ -6,10 +6,33 @@
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.HashMap;
+import java.util.HashSet;
+
 
 public class Monoalphabetic{
 
-    public static void main(String[] args){
+    private static String decrypt(String cipher, HashMap<String, String>){
 
+        return plainTxt;
+    }
+
+    private static void bruteforce(String cipher, ){
+        String str = "";
+        while(str.size > 0 && str.){
+
+        }
+        return str;
+    }
+
+    public static void main(String[] args){
+        bruteforce(args[1])
+        /*
+        try{
+            decrypt();
+        } catch(Exception e){
+            System.err.format("Exception occurred trying to read ")
+        }
+        */
     }
 }
