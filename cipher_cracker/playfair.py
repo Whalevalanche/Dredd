2dd87a4704d83be55bd71a31512a0ba6eeedccca
prijatelj <theomnipotentwoof@gmail.com>
2017-09-23 15:56:22 -0400

    playfair matrix made with np

--- cipher_cracker/playfair.py
+++ cipher_cracker/playfair.py
@@ -7,36 +7,49 @@ cipher, find the key of the PlayFair Cipher.
 
 import sys
 from collections import OrderedDict
+import numpy as np
 
 def encrypt(key, txt, letter_overlap = None):
     """
-    Encrypts the code using Playfair cipher with the given key.
+    Encrypts the code using Playfair cipher with the given key. doubles
+    insertion and single's addition letter is 'Q'
+
+    @param key: String of 25 unique letters
     """
     key = key.upper()
+    txt = txt.upper()
     key_unique = ''.join(OrderedDict.fromkeys(key).keys())
+
     if len(key_unique) < 25:
         # TODO Not provided with the full key, thus need to guess or use known
         # letter_overlap
-
-        # create playfair  5x5 table
-        #key += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-        alpha_key = ''.join(OrderedDict.fromkeys(key.upper()).keys())
+        unique_key += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        unique_key = ''.join(OrderedDict.fromkeys(unique_key).keys())
     else if len(unique_key) > 25:
         raise Exception("Given Key is greater than 25 unique characters")
+    else:
+        # The key is fully provided
+        # pick which alpha ordered letters to overlap: most freq = "st" or "ef"
+        #   most common digraph OR most common letter w/ its most common digraph
+        key_matrix = np.reshape(np.array(list(unique_key)), [5,5])
 
-    if len(alpha_key) > 25:
-        # There is single letter pair that overlaps
-        # if letter_overlap = None, throw error or search for overlapping chars
+    # get set of digraphs from plain text
+    #string length must be even
+    txt_digraph = Set()
+    for i in range(len(txt)):
+        if i % 2 != 0:
+            continue;
+        txt_digraph.add(txt[i:i+2]]);
 
-        """
-        TODO optimally, we should overlap, the alphabetical orderd pair, that
-        is most common. To be assholes & if allowed, our key will use the most
-        frequent digraph in all texts. Just trying to think how we
-        """
+    if len(txt) % 2 != 0:
+        txt_digraph.add("Q" + txt[-1])
+
+    if check_dubs(txt_digraph):
 
 
+    # for each unique digraph, get new mapping (create dict)
 
+    #convert string based on dict
 
     return encryption
 
@@ -45,14 +58,9 @@ def decrypt():
     Decrypts the provided cipher with the given key.
     """
 
-def crack(txt, cipher):
-    """
-    Given the plain text and the cipher, determines the key to the Playfair
-    cipher. Does so by simulating the potential cipher matrix.
-    """
-    # TODO need to find appropriate algorithm to determine key based on digraphs
-
-    # The key can be an assortment of 25 random and unique letters.
+def convert_dubs(txt):
+    for i, c in enumerate(txt):
+    return
 
 def main(args):
     """
